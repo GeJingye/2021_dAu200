35389550274ae963d4902e9973268ff3e8cbb0e9
JingyeGe <2598138720@qq.com>
2026-08-20 10:48:30 +0800

    20260819_dAu200_2021_TOF_Mee_0_4GeV.root

--- StRoot/2021.SL23d/StAnaCuts.h
+++ StRoot/2021.SL23d/StAnaCuts.h
@@ -18,7 +18,11 @@
 
 namespace anaCuts
 {
-	const std::array<UInt_t, 11> trigNumber = {880011};//zdce-mb
+	const std::array<UInt_t, 8> trigNumber = {880021,880011,880001,	880141,880131,880121,880111,880101};
+	//880021,880011,880001:zdce-mb;
+	//880022,880012,880002:mb-hltgood;
+	//880141,880131,880121,880111,880101:zdce-epde-central;
+	//880142,880132,880122,880112,880102:central-hltgood;
 	// event cuts 
 	Float_t const Vz_up = 70;
 	Float_t const Vz_low = -50;
